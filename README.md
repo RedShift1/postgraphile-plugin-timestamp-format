a8d04bcde02de698083fa00a6bd555a9d6b27090
RedShift1 <glenn@webmind.be>
2022-03-16 11:52:41 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # postgraphile-plugin-timestamp-format
+
+Adds formatting for `timestamptz` fields through
+[PostgreSQL's `to_char`](https://www.postgresql.org/docs/current/functions-formatting.html) function.
+
+![GraphiQL Screenshot](doc/graphiql_screenshot.png)
+
+## Installation
+Add the `date_format_tz` function to your database:
+
+```postgresql
+CREATE OR REPLACE FUNCTION
+    public.date_format_tz(
+        input_timestamp timestamp with time zone,
+        format text,
+        tz text
+    )
+    RETURNS text
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE STRICT PARALLEL UNSAFE
+AS $BODY$
+DECLARE result text;
+BEGIN
+	PERFORM set_config('TimeZone', tz, true);
+	result := to_char(input_timestamp, format);
+	RESET TimeZone;
+	RETURN result;
+END;
+$BODY$;
+```
+
+Add PostGraphilePluginTimestampFormat to appendPlugins:
+
+```js
+import express from 'express';
+import postgraphile from 'postgraphile';
+import PostGraphilePluginTimestampFormat from 'postgraphile-plugin-timestamp-format';
+
+const app = express();
+
+app.use(
+    postgraphile(
+        process.env.DATABASE_URL || 'postgres://user:pass@host:5432/dbname',
+        'public',
+        {
+            appendPlugins:
+            [
+                PostGraphilePluginTimestampFormat()
+            ],
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+        }
+    )
+);
+
+app.listen(process.env.PORT || 3000);
+
+```
+
+## API
+### PostGraphilePluginTimestampFormat({ [nameFn], [defaultFormat] })
+
+Constructs the plugin.
+
+* `nameFn` - `function(name: string): string`, called when a name is generated for the formatted timestamp field.
+  The returned string is used as the new field's name in the GraphQL schema
+* `defaultFormat` - `string`, Default timestamp format to use when called without a `format` argument
+
+## Supported timezones
+Which timezones are supported depends on your operating system. Run the following query
+on your database server to get a list:
+
+```postgresql
+SELECT name FROM pg_timezone_names() ORDER BY name;
+```
+
+## Default timezone
+When no tz argument is provided by the GraphQL query, the server's default timezone
+setting is used. Run the following query to know the default timezone your database
+server uses:
+
+```postgresql
+SELECT current_setting('TimeZone');
+```
---
+++ index.mjs
@@ -0,0 +1,158 @@
+function nullableIf(GraphQLNonNull, condition, Type)
+{
+    return condition ? Type : new GraphQLNonNull(Type);
+}
+
+/**
+ *
+ * @param {Object}      options
+ * @param {function}    options.nameFn
+ * @param {string}      options.defaultFormat Default format of the timestamp
+ * @returns {function(*): *}
+ */
+export default function(
+    {
+        nameFn = (name) => name + 'Formatted',
+        defaultFormat = 'YYYY-MM-DD"T"HH24:MI:SSOF' // ISO 8601
+    } = {}
+)
+{
+    return function(builder)
+    {
+        return builder.hook(
+            'GraphQLObjectType:fields',
+            (fields, build, context) =>
+            {
+                const {
+                    extend,
+                    pgSql: sql,
+                    pg2gqlForType,
+                    graphql: { GraphQLNonNull, GraphQLString },
+                    pgColumnFilter,
+                    inflection,
+                    pgOmit: omit,
+                    pgGetSelectValueForFieldAndTypeAndModifier,
+                    describePgEntity,
+                    sqlCommentByAddingTags,
+                } = build;
+
+                const {
+                    scope: { isPgRowType, isPgCompoundType, pgIntrospection: table },
+                    fieldWithHooks,
+                } = context;
+
+                if (
+                    !(isPgRowType || isPgCompoundType) ||
+                    !table ||
+                    table.kind !== 'class'
+                ) {
+                    return fields;
+                }
+
+                return extend(
+                    fields,
+                    table.attributes.reduce((memo, attr) => {
+                        if (!pgColumnFilter(attr, build, context))
+                            return memo;
+
+                        if (omit(attr, 'read'))
+                            return memo;
+
+                        if(attr.type.name !== 'timestamptz')
+                            return memo;
+
+                        // Exclude columns that are primary key
+                        if(table.primaryKeyConstraint && table.primaryKeyConstraint.keyAttributeNums.includes(attr.num))
+                            return memo;
+
+                        // Exclude columns that are foreign keys
+                        if(table.constraints.some(constraint => constraint.type === 'f' && constraint.keyAttributeNums.includes(attr.num)))
+                            return memo;
+
+                        const fieldName = nameFn(inflection.column(attr));
+
+                        if (memo[fieldName])
+                        {
+                            throw new Error(
+                                `Two columns produce the same GraphQL field name '${fieldName}' on class '${table.namespaceName}.${table.name}'; one of them is '${attr.name}'`
+                            );
+                        }
+
+                        memo = extend(
+                            memo,
+                            {
+                                [fieldName]: fieldWithHooks(
+                                    fieldName,
+                                    fieldContext => {
+                                        const { type, typeModifier } = attr;
+                                        const sqlColumn = sql.identifier(attr.name);
+                                        const { addDataGenerator } = fieldContext;
+                                        const ReturnType = GraphQLString;
+
+                                        addDataGenerator(
+                                            parsedResolveInfoFragment =>
+                                            {
+                                                const { args } = parsedResolveInfoFragment;
+                                                const tz = args.tz ? sql.value(args.tz) : sql.fragment`current_setting('TimeZone')`;
+                                                const format = args.format ? sql.value(args.format) : sql.value(defaultFormat);
+
+                                                return {
+                                                    pgQuery: queryBuilder =>
+                                                    {
+                                                        queryBuilder.select(
+                                                            pgGetSelectValueForFieldAndTypeAndModifier(
+                                                                ReturnType,
+                                                                fieldContext,
+                                                                parsedResolveInfoFragment,
+                                                                sql.fragment`(public.date_format_tz(${queryBuilder.getTableAlias()}.${sqlColumn}, ${format}, ${tz}))`, // The brackets are necessary to stop the parser getting confused, ref: https://www.postgresql.org/docs/9.6/static/rowtypes.html#ROWTYPES-ACCESSING
+                                                                type,
+                                                                typeModifier
+                                                            ),
+                                                            fieldName + parsedResolveInfoFragment.alias
+                                                        );
+                                                    },
+                                                };
+                                            }
+                                        );
+
+                                        const convertFromPg = pg2gqlForType(type);
+
+                                        return {
+                                            args:
+                                            {
+                                                format: { type: GraphQLString, description: 'See https://www.postgresql.org/docs/current/functions-formatting.html' },
+                                                tz: { type: GraphQLString, description: 'Timezone to format the date in' }
+                                            },
+                                            description: attr.description,
+                                            type: nullableIf(
+                                                GraphQLNonNull,
+                                                !attr.isNotNull &&
+                                                !attr.type.domainIsNotNull &&
+                                                !attr.tags.notNull,
+                                                ReturnType
+                                            ),
+                                            resolve(data, _args, _ctx, _info)
+                                            {
+                                                return convertFromPg(data[fieldName + _info.path.key]);
+                                            }
+                                        };
+                                    },
+                                    { pgFieldIntrospection: attr }
+                                ),
+                            },
+                            `Adding field for ${describePgEntity(
+                                attr
+                            )}. You can rename this field with a 'Smart Comment':\n\n  ${sqlCommentByAddingTags(
+                                attr,
+                                { name: "newNameHere" }
+                            )}`
+                        );
+                        return memo;
+                    }, {}),
+                    `Adding columns to '${describePgEntity(table)}'`
+                );
+            },
+            ['PgColumns']
+        );
+    }
+}
